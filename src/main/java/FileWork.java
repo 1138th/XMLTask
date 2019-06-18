2ca1049950356d4def025f47b6918b52cb77d213
Ansar.Daku <dakuansar7@gmail.com>
2019-06-18 19:06:18 +0600

    Added:  - xml-file creating;  - filling information about files;  - folder size counting.

---
+++ src/main/java/FileWork.java
@@ -0,0 +1,56 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+class FileWork {
+
+    private static long folderSize(File directory){
+        long length = 0;
+        for (File file : directory.listFiles()) {
+            if (file.isFile()){
+                length += file.length();
+            } else{
+                length += folderSize(file);
+            }
+        }
+        return length;
+    }
+
+    private static void folderContent(File directory, Writer writer, BasicFileAttributes attributes, SimpleDateFormat dateFormat) throws Exception{
+        for (File file : directory.listFiles()) {
+            if (file.isFile()){
+                writer.write("\t\t<file name=\"" + file.getName() +
+                        "\" created=\"" + dateFormat.format(new Date(attributes.creationTime().toMillis())) +
+                        "\" size=\"" + attributes.size() + "\" />\n");
+            } else{
+                folderSize(file);
+            }
+        }
+    }
+
+    static void xmlFiller(File directory, BufferedWriter writer) throws Exception{
+        writer.write("<data>\n");
+        BasicFileAttributes attributes;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        for (File directoryItem : directory.listFiles()) {
+            attributes = Files.readAttributes(directoryItem.toPath(), BasicFileAttributes.class);
+            if (directoryItem.isFile()){
+                writer.write("\t<file name=\"" + directoryItem.getName() +
+                        "\" created=\"" + dateFormat.format(new Date(attributes.creationTime().toMillis())) +
+                        "\" size=\"" + attributes.size() + "\" />\n");
+            }
+            if (directoryItem.isDirectory()){
+                writer.write("\t<dir name=\"" + directoryItem.getName() +
+                        "\" created=\"" + dateFormat.format(new Date(attributes.creationTime().toMillis())) +
+                        "\" size=\"" + folderSize(directoryItem) + "\" />\n");
+                folderContent(directoryItem, writer, attributes, dateFormat);
+            }
+        }
+        writer.write("</data>");
+
+    }
+}
--- src/main/java/XMLTask.java
+++ src/main/java/XMLTask.java
@@ -1,2 +1,18 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 public class XMLTask {
+    public static void main(String[] args) {
+        File xmlFile = new File("src\\XMLTask.xml");
+        File directory = new File("src");
+        try {
+            if (xmlFile.createNewFile()) System.out.println("File has been successfully created.");
+            BufferedWriter writer = new BufferedWriter(new FileWriter(xmlFile));
+            FileWork.xmlFiller(directory, writer);
+            writer.close();
+        } catch (Exception e){
+            System.out.println("Haven't access to create or edit file.");
+        }
+    }
 }
