7b2d71da7609f09df297d69669b0f6f02e466134
Ansar.Daku <dakuansar7@gmail.com>
2019-06-18 23:12:51 +0600

    Added filling current directory methods that includes:  - creating 3 folders;  - putting in each folder 3 .txt files;  - writing in every single file random quantity of symbols with the following rules:    - number of lines generating randomly in quantity from 50 to 150;    - number of symbols in each line generating randomly in quantity from 50 to 150;    - each symbol might be (values are taken in range 48-122 from ASCII table):      - digits from 0 to 9;      - capital and lowercase letters;      - some special symbols (:;<=>?@[\]^_`).

--- src/main/java/FileWork.java
+++ src/main/java/FileWork.java
@@ -1,12 +1,75 @@
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 
 class FileWork {
+
+    static void fillXML(File directory, BufferedWriter writer) throws Exception{
+        writer.write("<data>\n");
+        BasicFileAttributes attributes;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        for (File directoryItem : directory.listFiles()) {
+            attributes = Files.readAttributes(directoryItem.toPath(), BasicFileAttributes.class);
+            if (directoryItem.isFile()){
+                writer.write("\t<file name=\"" + directoryItem.getName() +
+                        "\" created=\"" + dateFormat.format(new Date(attributes.creationTime().toMillis())) +
+                        "\" size=\"" + attributes.size() + "\" />\n");
+            }
+            if (directoryItem.isDirectory()){
+                writer.write("\t<dir name=\"" + directoryItem.getName() +
+                        "\" created=\"" + dateFormat.format(new Date(attributes.creationTime().toMillis())) +
+                        "\" size=\"" + folderSize(directoryItem) + "\" />\n");
+                folderContent(directoryItem, writer, attributes, dateFormat);
+            }
+        }
+        writer.write("</data>");
+    }
+
+    static void fillDirectory(){
+        File taskFolder;
+        File taskFile;
+        BufferedWriter writer;
+
+        for (int i = 0; i < 3; i++){
+            taskFolder = new File("src\\TaskFolder" + (i + 1));
+            if (taskFolder.mkdir()) System.out.println("Directory " + taskFolder.getName() + " has been created");
+
+            for (int j = 0; j < 3; j++){
+                taskFile = new File(taskFolder + "\\TaskFile" + (i + 1) + "" + (j + 1) + ".txt");
+                try {
+                    if (taskFile.createNewFile())
+                        System.out.println("File " + taskFile.getName() + " in folder " +
+                                taskFolder.getName() + " has been created");
+                    writer = new BufferedWriter(new FileWriter(taskFile));
+                    writer.write("Text in "  + taskFile + " creating automatically.\n");
+                    generateRandText(writer);
+                    writer.close();
+                } catch (Exception e){
+                    System.out.println("Haven't access to create or edit file.");
+                    return;
+                }
+            }
+        }
+    }
+
+    private static void generateRandText(Writer writer) throws Exception{
+        Random rand = new Random();
+        int numberOfLines = rand.nextInt(100) + 50;
+        int symbolsInLine = rand.nextInt(100) + 50;
+        //char: 48-122 - numbers, letters and some special symbols
+        for (int lines = 0; lines < numberOfLines; lines++){
+            for (int symbol = 0; symbol < symbolsInLine; symbol++){
+                writer.write(((char) (rand.nextInt(74) + 48)) + " ");
+            }
+            writer.write("\n");
+        }
+    }
 
     private static long folderSize(File directory){
         long length = 0;
@@ -31,27 +94,5 @@ class FileWork {
                 folderContent(file, writer, attributes, dateFormat);
             }
         }
-    }
-
-    static void xmlFiller(File directory, BufferedWriter writer) throws Exception{
-        writer.write("<data>\n");
-        BasicFileAttributes attributes;
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        for (File directoryItem : directory.listFiles()) {
-            attributes = Files.readAttributes(directoryItem.toPath(), BasicFileAttributes.class);
-            if (directoryItem.isFile()){
-                writer.write("\t<file name=\"" + directoryItem.getName() +
-                        "\" created=\"" + dateFormat.format(new Date(attributes.creationTime().toMillis())) +
-                        "\" size=\"" + attributes.size() + "\" />\n");
-            }
-            if (directoryItem.isDirectory()){
-                writer.write("\t<dir name=\"" + directoryItem.getName() +
-                        "\" created=\"" + dateFormat.format(new Date(attributes.creationTime().toMillis())) +
-                        "\" size=\"" + folderSize(directoryItem) + "\" />\n");
-                folderContent(directoryItem, writer, attributes, dateFormat);
-            }
-        }
-        writer.write("</data>");
-
     }
 }
--- src/main/java/XMLTask.java
+++ src/main/java/XMLTask.java
@@ -4,12 +4,13 @@ import java.io.FileWriter;
 
 public class XMLTask {
     public static void main(String[] args) {
-        File xmlFile = new File("src\\XMLTask.xml");
+        File XMLFile = new File("src\\XMLTask.xml");
         File directory = new File("src");
         try {
-            if (xmlFile.createNewFile()) System.out.println("File has been successfully created.");
-            BufferedWriter writer = new BufferedWriter(new FileWriter(xmlFile));
-            FileWork.xmlFiller(directory, writer);
+            if (XMLFile.createNewFile()) System.out.println("File has been successfully created.");
+            BufferedWriter writer = new BufferedWriter(new FileWriter(XMLFile));
+            FileWork.fillDirectory();
+            FileWork.fillXML(directory, writer);
             writer.close();
         } catch (Exception e){
             System.out.println("Haven't access to create or edit file.");
